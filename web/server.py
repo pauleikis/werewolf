31700e92969ac0ccfc094b71afe7d3589c4ce221
Paulius Šileikis <paulius.sileikis@gmail.com>
2015-01-20 21:17:56 +0200

    Page available externally

--- web/server.py
+++ web/server.py
@@ -3,7 +3,10 @@ from ww.index import ww
 
 
 app = Flask(__name__)
+app.config.from_pyfile('config/default.cfg')
+app.config.from_envvar('WEREWOLF_SETTINGS', silent=True)
+
 app.register_blueprint(ww)
 
 if __name__ == "__main__":
-    app.run(host="10.0.1.8", debug=True)
+    app.run(host="0.0.0.0")
